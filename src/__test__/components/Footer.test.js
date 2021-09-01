80a002cdcc2a0dd336f7419a055d2ca03d838d9d
John Fabio Giraldo <codigojohn@gmail.com>
2021-09-01 00:42:11 -0500

    Updated Footer test

--- src/__test__/components/Footer.test.js
+++ src/__test__/components/Footer.test.js
@@ -12,7 +12,7 @@ describe('<Footer />', () => {
   });
 
   test('Render del título', () => {
-    expect(footer.find('.Footer-title').text()).toEqual('Platzi Store');
+    expect(footer.find('.Footer-title').text()).toEqual('Platzi Stores');
   });
 });
 
